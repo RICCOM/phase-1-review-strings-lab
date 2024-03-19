cd92d3669b3e4f29b3a885dffa8a0480b739ea73
RICCOM <ericomun@gmail.comgit>
2024-03-19 20:32:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Munjuri Eric';
+const welcomeMessage = 'Welcome to Flatbook, ';
+const welcomeMessage = currentUser;
+const welcomeMessage = 'Welcome to Flatbook, currentUser';
+const welcomeMessage ='Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = '';
+const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
